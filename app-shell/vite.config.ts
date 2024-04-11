d5bc2396df67d8d60bc404a85391df43b6f97463
PhucNguyenHoang <hoangphucnguyen112002@gmail.com>
2024-04-12 00:07:14 +0700

    Auth - Create Vue project

--- app-shell/vite.config.ts
+++ app-shell/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,7 +12,12 @@ export default defineConfig({
       remotes: {
 
       },
-      shared: []
+      shared: ["react", "react-dom"]
     })
-  ]
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
+  }
 });
---
+++ auth/src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import Button from "primevue/button";
+import Card from 'primevue/card';
+</script>
+
+<template>
+    <h1 class="text-red-500 font-bold">PrimeVue Components</h1>
+
+    <Button label="Primary" class="" />
+    <Button label="Secondary" severity="secondary" />
+    <Button label="Success" severity="success" />
+    <Button label="Info" severity="info" />
+    <Button label="Warning" severity="warning" />
+    <Button label="Help" severity="help" />
+    <Button label="Danger" severity="danger" />
+
+    <Card>
+      <template #title> Simple Card </template>
+      <template #content>
+          <p class="m-0">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
+              quas!
+          </p>
+      </template>
+    </Card>
+</template>
+
+<style scoped>
+
+</style>
