cde165ce37ff0b66dfc2701099f5aa876a3d0e6c
PhucNguyenHoang <hoangphucnguyen112002@gmail.com>
2024-04-14 16:09:34 +0700

    Auth - Sign Up page

--- auth/src/pages/SignIn.vue
+++ auth/src/pages/SignIn.vue
@@ -5,16 +5,14 @@ import { RouterLink } from "vue-router";
 import SignInImage from "/SignInImage.jpg";
 
 const $toast = useToast();
+const emailRegex = /^[a-z0-9_\.]{1,32}@[a-z0-9]{2,10}(\.[a-z0-9]{2,10}){1,}$/;
 
 const inputRules: any = {
   emailRequired: (value: string) => !!value || "Email is required",
-  emailFormat: (value: string) => {
-    const emailRegex = /^[a-z0-9_\.]{1,32}@[a-z0-9]{2,10}(\.[a-z0-9]{2,10}){1,}$/;
-    return emailRegex.test(value) || "Invalid email format";
-  },
+  emailFormat: (value: string) => emailRegex.test(value) || "Invalid email format",
   passwordRequired: (value: string) => !!value || "Password is required",
   passwordLength: (value: string) => value.length >= 8 || "Password must be at least 8 characters long"
-}
+};
 
 const isLoading = ref(false);
 const email = ref("");
@@ -22,11 +20,11 @@ const password = ref("");
 const showPassword = ref(false);
 const rememberMe = ref(false);
 
-const handleLogin = () => {
+const handleSignIn = () => {
   isLoading.value = true;
   
-  if (email.value !== "" && password.value !== "") {
-    showToast("success", "Login Successfully !!!")
+  if (email.value !== "" && emailRegex.test(email.value) && password.value !== "") {
+    showToast("success", "Sign In Successfully !!!");
   }
   
   isLoading.value = false;
@@ -47,11 +45,11 @@ const showToast = (errorType: string, message = "") => {
       <img :src="SignInImage" alt="WonderTripSignIn" class="object-cover w-full h-full">
     </div>
 
-    <div class="w-full lg:w-3/5 login-flex-container flex-col p-6">
-      <div class="w-full login-flex-container flex-col mb-10">
-        <div class="login-flex-container">
+    <div class="w-full lg:w-3/5 flexbox-col p-6">
+      <div class="w-full flexbox-col mb-10">
+        <div class="flexbox-row">
           <p class="text-3xl mr-2 font-weight-bold">
-            Welcome back, Travelers !
+            Welcome back, Travelers
           </p>
           <v-icon icon="mdi-hand-wave" color="#FBD964" size="x-large"></v-icon>
         </div>
@@ -60,9 +58,10 @@ const showToast = (errorType: string, message = "") => {
         </p>
       </div>
       <div class="max-w-md w-full">
-        <v-form @submit.prevent="handleLogin" class="mt-8">
+        <v-form @submit.prevent="handleSignIn" class="mt-8">
           <v-text-field
             class="mb-4"
+            type="email"
             :rules="[inputRules.emailRequired, inputRules.emailFormat]"
             label="Email Address"
             v-model="email"
@@ -80,7 +79,7 @@ const showToast = (errorType: string, message = "") => {
           </v-text-field>
           
           <div class="flex justify-between items-center">
-            <div class="login-flex-container">
+            <div class="flexbox-row">
               <v-checkbox-btn
                 color="rgb(var(--primary-1))"
                 v-model="rememberMe">
@@ -100,28 +99,28 @@ const showToast = (errorType: string, message = "") => {
             class="mt-12 text-white"
             color="rgb(var(--primary-1))"
             type="submit">
-            LOGIN
+            SIGN IN
           </v-btn>
         </v-form>
 
-        <div class="login-flex-container mt-2">
+        <div class="flexbox-row mt-2">
           <p class="mr-2 text-sm">
             Don't have an account yet ?
           </p>
           <RouterLink to="/sign-up">
             <p class="font-weight-bold text-primary-1 text-sm">
-              Register now
+              Sign up now
             </p>
           </RouterLink>
         </div>
 
-        <div class="login-flex-container my-10">
+        <div class="flexbox-row my-10">
           <v-divider :thickness="2" class="border-opacity-75"></v-divider>
           <p class="mx-2 text-gray-400">or</p>
           <v-divider :thickness="2" class="border-opacity-75"></v-divider>
         </div>
 
-        <div class="login-flex-container">
+        <div class="flexbox-row">
           <v-btn
             rounded
             class="text-none w-full"
@@ -137,9 +136,5 @@ const showToast = (errorType: string, message = "") => {
 </template>
 
 <style scoped>
-.login-flex-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-}
+
 </style>
